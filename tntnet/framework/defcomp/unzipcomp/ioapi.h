507ea339e3f510ab18557870bd7c1d06b0c9177b
tommi <tommi@8dd45c8f-be11-0410-86c3-e0da43b70fc1>
2004-10-18 20:55:51 +0000

    corrected copyright-entries
    git-svn-id: svn://svn.code.sf.net/p/tntnet/code/trunk@94 8dd45c8f-be11-0410-86c3-e0da43b70fc1

--- tntnet/framework/defcomp/unzipcomp/ioapi.h
+++ tntnet/framework/defcomp/unzipcomp/ioapi.h
@@ -1,22 +1,9 @@
-/* ioapi.h
-   Copyright (C) 2003 Tommi Mäkitalo
+/* ioapi.h -- IO base function header for compress/uncompress .zip
+   files using zlib + zip or unzip API
 
-This file is part of tntnet.
+   Version 1.00, September 10th, 2003
 
-Tntnet is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-Tntnet is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with tntnet; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330,
-Boston, MA  02111-1307  USA
+   Copyright (C) 1998-2003 Gilles Vollant
 */
 
 #ifndef _ZLIBIOAPI_H
--- tntnet/framework/defcomp/unzipcomp/unzip.h
+++ tntnet/framework/defcomp/unzipcomp/unzip.h
@@ -1,22 +1,36 @@
-/* unzip.h
-   Copyright (C) 2003 Tommi Mäkitalo
+/* unzip.h -- IO for uncompress .zip files using zlib
+   Version 1.00, September 10th, 2003
 
-This file is part of tntnet.
+   Copyright (C) 1998-2003 Gilles Vollant
 
-Tntnet is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+   This unzip package allow extract file from .ZIP file, compatible with PKZip 2.04g
+     WinZip, InfoZip tools and compatible.
+   Encryption and multi volume ZipFile (span) are not supported.
+   Old compressions used by old PKZip 1.x are not supported
 
-Tntnet is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with tntnet; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330,
-Boston, MA  02111-1307  USA
+   I WAIT FEEDBACK at mail info@winimage.com
+   Visit also http://www.winimage.com/zLibDll/unzip.htm for evolution
+
+   Condition of use and distribution are the same than zlib :
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+
+
 */
 
 /* for more info about .ZIP format, see
