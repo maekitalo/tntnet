f725be6abe040ece53229c5bc2ad3c1d7fd9b576
tommi <tommi@8dd45c8f-be11-0410-86c3-e0da43b70fc1>
2005-05-25 16:02:54 +0000

    GNU-License added
    git-svn-id: svn://svn.code.sf.net/p/tntnet/code/trunk@292 8dd45c8f-be11-0410-86c3-e0da43b70fc1

--- tntnet/framework/common/tnt/object.h
+++ tntnet/framework/common/tnt/object.h
@@ -1,6 +1,23 @@
-////////////////////////////////////////////////////////////////////////
-// tnt/object.h
-//
+/* tnt/object.h
+   Copyright (C) 2005 Tommi Maekitalo
+
+This file is part of tntnet.
+
+Tntnet is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+Tntnet is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with tntnet; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330,
+Boston, MA  02111-1307  USA
+*/
 
 #ifndef TNT_OBJECT_H
 #define TNT_OBJECT_H
