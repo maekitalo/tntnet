df5b27001fd7aa61c61a3031d1dc1ecae6ab3525
maekitalo <maekitalo@8dd45c8f-be11-0410-86c3-e0da43b70fc1>
2007-03-22 19:31:02 +0000

    change license from GPL to LGPL
    git-svn-id: svn://svn.code.sf.net/p/tntnet/code/trunk@769 8dd45c8f-be11-0410-86c3-e0da43b70fc1

--- tntnet/framework/common/langlib.cpp
+++ tntnet/framework/common/langlib.cpp
@@ -1,21 +1,22 @@
 /* langlib.cpp
  * Copyright (C) 2003-2005 Tommi Maekitalo
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but
- * is provided AS IS, WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, and
- * NON-INFRINGEMENT.  See the GNU General Public License for more details.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
+
 
 #include "tnt/langlib.h"
 #include <cxxtools/log.h>
