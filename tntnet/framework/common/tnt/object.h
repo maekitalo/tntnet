4f451ae2533ed0dbb8ddc8392af003736a11e73b
maekitalo <maekitalo@8dd45c8f-be11-0410-86c3-e0da43b70fc1>
2012-02-09 18:49:04 +0000

    fix declaration template class (clang complained about it)
    git-svn-id: svn://svn.code.sf.net/p/tntnet/code/trunk@1316 8dd45c8f-be11-0410-86c3-e0da43b70fc1

--- tntnet/framework/common/tnt/object.h
+++ tntnet/framework/common/tnt/object.h
@@ -57,7 +57,7 @@ namespace tnt
         : ptr(ptr_)
         { }
       ~PointerObject()
-        { destroy(ptr); }
+        { destroyPolicy<data_type>::destroy(ptr); }
       void set(data_type* ptr_)
         { destroy(ptr); ptr = ptr_; }
       data_type* get()  { return ptr; }
