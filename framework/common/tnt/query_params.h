255886ac51e4b493c53856014c5aaaf0d65cb67a
Tommi Mäkitalo <tommi@tntnet.org>
2023-04-25 07:55:22 +0200

    fix for compiling with C++17 and recent cxxtools changes
    std::iterator is deprecated in C++17

--- framework/common/tnt/query_params.h
+++ framework/common/tnt/query_params.h
@@ -286,9 +286,11 @@ inline std::string QArg<std::string>::get(const QueryParams& q, const std::strin
 
 inline std::vector<std::string> QArg<std::string>::getvector(const QueryParams& q, const std::string& name)
 {
-    return std::vector<std::string>(q.begin(name + "[]"), q.end());
+    std::vector<std::string> v;
+    for (auto it = q.begin(name + "[]"); it != q.end(); ++it)
+        v.emplace_back(*it);
+    return v;
 }
-
 
 ////////////////////////////////////////////////////////////////////////
 // specialization for char
